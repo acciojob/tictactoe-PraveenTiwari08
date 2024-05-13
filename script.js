cd06b3cd0c8dacd2d44a9a951f418f1c72d3e3c4
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-13 17:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function Startgame() {
+	 var inputDiv = document.getElementById("inputdiv");
+    if (inputDiv) {
+        inputDiv.remove();
+		
+    }
+	var gamePage = document.createElement("div");
+    gamePage.className = "gamepage";
+    document.body.appendChild(gamePage);
+}
